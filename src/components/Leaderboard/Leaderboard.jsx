ca2a3d8734d949512435f40dba84bc6897138621
Owner <aryanjairath7@gmail.com>
2025-02-10 23:10:00 -0500

    back button

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -3,8 +3,12 @@ import { useState } from 'react';
 import React from 'react';
 import './Leaderboard.css'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 const Leaderboard  = () => {
     const [data, setData] = useState([])
+    const navigate = useNavigate();
+
     useEffect (() => {
         const setLeaderboard =  async () => {
             const response = await axios.get('http://localhost:5000/api/leaderboard')
@@ -13,9 +17,16 @@ const Leaderboard  = () => {
         };
         setLeaderboard();
     }, []);
+
+    const handleBack = () => {
+        navigate('/game')
+    }
     return (
         <div className='leaderboard'>
-            <h1>Leaderboard</h1>
+            <div classname = "header">
+                <button onClick = {handleBack}>Go back to the main menu?</button>
+                <h1>Leaderboard</h1>
+            </div>
             {data.map(item =><div className='leader-item'><div className='user'><h1>{item.username}</h1></div> <h2>Time spent: {item.time} seconds</h2> <h2>Score: {item.score}</h2> <h2> Number of Questions: {item.totalQuestions}</h2></div>)}
         </div>
     );
